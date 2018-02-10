041da264c2d7a64c36cfe24092d5c6293654eb4d
damoiser <dam.radice@gmail.com>
2018-02-10 19:30:45 +0100

    help the usr: retrieve all the vaults

--- glacier/remove_single_vault_and_all_archives.py
+++ glacier/remove_single_vault_and_all_archives.py
@@ -6,6 +6,15 @@ print("THIS SCRIPT IS STILL IN WORK IN PROGRESS - DONT USE IT YET")
 print("this script will delete all archives and the vault, see glacier/README.md for further details how it works")
 
 
+account_id = input("enter account id (blank if same as configured in aws): ")
+if account_id == "":
+  # if using the default one use dash
+  account_id = "-"
+
+
+aws_vaults = subprocess.run(['aws', 'glacier', 'list-vaults', '--account-id', account_id], stdout=subprocess.PIPE).stdout.decode('utf-8').strip('\n')
+
+
 while True:
   vault_name = input("enter vault name: ")
 
@@ -14,10 +23,6 @@ while True:
   else:
     break
 
-account_id = input("enter account id (blank if same as configured in aws): ")
-if account_id == "":
-  # default one is dash
-  account_id = "-"
 
 aws_region = subprocess.run(['aws', 'configure', 'get', 'region'], stdout=subprocess.PIPE).stdout.decode('utf-8').strip('\n')
 
@@ -25,4 +30,5 @@ region = input("enter region (blank for " + aws_region + "): ")
 
 if region == "":
   region = aws_region
+
 
