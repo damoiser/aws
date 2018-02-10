bb2aed56c4936d20e84d78856858047b53f4593c
Damiano Radice <damiano.radice@inwork.ch>
2018-02-10 19:31:06 +0100

    begin with the main and aux funcs

--- glacier/remove_single_vault_and_all_archives.py
+++ glacier/remove_single_vault_and_all_archives.py
@@ -32,3 +32,28 @@ if region == "":
   region = aws_region
 
 
+
+
+
+# main processes
+
+def init_inventory_job(account_id, vault_name, region):
+  print("starting init inventory job, after the initialization you need to wait some hours to wait that AWS finish the job, you can rerun the job to check the status")
+  subprocess.run(['aws', 'glacier', 'initiate-job', '--job-parameters', '\'{"Type": "inventory-retrieval"}\'', '--vault-name', vault_name, '--account-id', account_id, '--region', region])
+
+def get_inventory_result(vault_name, region):
+
+
+def delete_archives():
+
+# aux functions
+
+def job_tmp_filename(vault_name, region):
+  return str(region) + "_" + str(vault_name) + ".job"
+
+def job_result_filename(vault_name, region, job_id):
+  return str(region) + "_" + str(vault_name) + "_" + str(job_id) + ".json"
+
+def get_job_id_from_file(vault_name, region):
+  
+def is_job_finished(account_id, vault_name):
