6639d9ba0edfd8431aa31df5dba8ea060924b150
Damiano Radice <damiano.radice@inwork.ch>
2018-02-11 12:25:10 +0100

    defining base dir struct + create tmp file

--- glacier/remove_single_vault_and_all_archives.py
+++ glacier/remove_single_vault_and_all_archives.py
@@ -1,5 +1,5 @@
 
-import subprocess, json, sys
+import subprocess, json, sys, os
 
 ########################################
 # functions
@@ -9,20 +9,35 @@ import subprocess, json, sys
 
 def init_inventory_job(account_id, vault_name, region):
   print("starting init inventory job, after the initialization you need to wait some hours to wait that AWS finish the job, you can rerun the job to check the status")
-  subprocess.run(['aws', 'glacier', 'initiate-job', '--job-parameters', '\'{"Type": "inventory-retrieval"}\'', '--vault-name', vault_name, '--account-id', account_id, '--region', region])
+  # aws_job_response = json.loads(subprocess.run(['aws', 'glacier', 'initiate-job', '--job-parameters', '\'{"Type": "inventory-retrieval"}\'', '--vault-name', vault_name, '--account-id', account_id, '--region', region], stdout=subprocess.PIPE).stdout.decode('utf-8').strip('\n'))
+
+  # job_id = aws_job_response["jobId"]
+  job_id = "bla"
+
+  print("job for vault " + vault_name + " initiated (job id: "  + job_id + "), job id stored in " + job_tmp_filename(vault_name, region))
+
+  job_file = open(job_tmp_filename(vault_name, region), "w")
+  job_file.write(job_id)
+  job_file.close()
+
 
 # def get_inventory_result(vault_name, region):
 
 
 # def delete_archives():
 
-# aux functions
+# AUX FUNCS
+
+def create_dir_structure():
+  if not os.path.exists("jobs"):
+    os.makedirs("jobs")
+
 
 def job_tmp_filename(vault_name, region):
-  return str(region) + "_" + str(vault_name) + ".job"
+  return "jobs/" + str(region) + "__" + str(vault_name) + ".job"
 
 def job_result_filename(vault_name, region, job_id):
-  return str(region) + "_" + str(vault_name) + "_" + str(job_id) + ".json"
+  return "jobs/" + str(region) + "__" + str(vault_name) + "__" + str(job_id) + ".json"
 
 # def get_job_id_from_file(vault_name, region):
   
@@ -54,6 +69,7 @@ print("THIS SCRIPT IS STILL IN WORK IN PROGRESS - DONT USE IT YET")
 
 print("this script will delete all archives and the vault, see glacier/README.md for further details how it works")
 
+create_dir_structure()
 
 account_id = input("enter account id (blank if same as configured in aws): ")
 if account_id == "":
