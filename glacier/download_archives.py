f47cca82279905deaf923f00770852ea0cc0e064
Damiano Radice <dam.radice@gmail.com>
2020-06-14 21:43:05 +0200

    begin with download job + download archive

--- glacier/download_archives.py
+++ glacier/download_archives.py
@@ -24,6 +24,8 @@ def select_what_download(account_id, vault_name, region):
 
     elif nr == "1":
       archive_id = input("provide archive id as present in the inventory list")
+      start_or_check_retrieval_job(archive_id)
+
       download_single_archive(archive_id)
     elif nr == "2":
       print("[TODO] retrieve range of files from inventory list (index_start, index_end)")
@@ -33,7 +35,16 @@ def select_what_download(account_id, vault_name, region):
       print("value not recognized...try again")
 
 
-def download_single_archive(archive_id):
+def start_or_check_retrieval_job(archive_id):
+  # check if job is already running
+  job_files = glob.glob(aux.get_download_job_filename(vault_name, region, [archive_id]))
+  if len(job_files) > 0:
+    answer = input("it seems that a download job is already running", aux.get_download_job_filename(vault_name, region, [archive_id]), "start the download? [y,n]:")
+    if answer.lower() == "y" or answer.lower() == "yes":
+      download_single_archive(archive_id)
+      print("download done, exiting...")
+      sys.exit(0)
+
  #  aws glacier initiate-job --account-id - --vault-name my-vault --job-parameters file://job-archive-retrieval.json
  #  job-archive-retrieval.json  is  a  JSON  file  in the local folder that
  #  specifies the type of job, archive ID, and some optional parameters:
@@ -49,15 +60,20 @@ def download_single_archive(archive_id):
     "ArchiveId": archive_id,
   }
 
-  aws_job_response = json.loads(subprocess.run(['aws', 'glacier', 'initiate-job', '--account-id', account_id, '--vault-name', vault_name, '--job-parameters', json.dumps(request_parameters), job_result_filename(vault_name, region)], stdout=subprocess.PIPE).stdout.decode('utf-8').strip('\n')
-  
+  aws_job_response = json.loads(subprocess.run(['aws', 'glacier', 'initiate-job', '--account-id', account_id, '--vault-name', vault_name, '--job-parameters', json.dumps(request_parameters), job_result_filename(vault_name, region)], stdout=subprocess.PIPE).stdout.decode('utf-8').strip('\n'))
+
   job_id = aws_job_response["jobId"]
 
-  print("job for vault " + vault_name + " initiated (job id: "  + job_id + "), job id stored in " + job_tmp_filename(vault_name, region))
+  print("job for vault " + vault_name + " initiated (job id: "  + job_id + "), job id stored in " + aux.get_download_job_filename(vault_name, region, [archive_id]))
 
-  job_file = open(job_tmp_filename(vault_name, region), "w")
+  job_file = open(aux.get_download_job_filename(vault_name, region, [archive_id]), "w")
   job_file.write(job_id)
   job_file.close()
+
+
+def download_single_archive(archive_id):
+  print("start download request for archive_id:", archive_id)
+
 
 ########################################
 # init
