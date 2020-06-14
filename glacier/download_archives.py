865af4a854b5d5e99fd618449ec9d65e57c33b1f
Damiano Radice <dam.radice@gmail.com>
2020-06-14 22:09:43 +0200

    download archive first try

--- glacier/download_archives.py
+++ glacier/download_archives.py
@@ -12,6 +12,8 @@ def select_what_download(account_id, vault_name, region):
   print("3. all vault (all result set)")
   nr = input("Put a number")
 
+  # create base vault download path
+  os.makedirs(os.path.join("downloads", vault_name))
 
   while True:
     if nr == "0":
@@ -24,9 +26,7 @@ def select_what_download(account_id, vault_name, region):
 
     elif nr == "1":
       archive_id = input("provide archive id as present in the inventory list")
-      start_or_check_retrieval_job(archive_id)
-
-      download_single_archive(archive_id)
+      start_or_check_retrieval_job([archive_id])
     elif nr == "2":
       print("[TODO] retrieve range of files from inventory list (index_start, index_end)")
     elif nr == "3":
@@ -35,15 +35,45 @@ def select_what_download(account_id, vault_name, region):
       print("value not recognized...try again")
 
 
-def start_or_check_retrieval_job(archive_id):
+def start_or_check_retrieval_job(archive_ids):
   # check if job is already running
-  job_files = glob.glob(aux.get_download_job_filename(vault_name, region, [archive_id]))
+  job_files = glob.glob(aux.get_download_job_filename(vault_name, region, archive_ids))
   if len(job_files) > 0:
-    answer = input("it seems that a download job is already running", aux.get_download_job_filename(vault_name, region, [archive_id]), "start the download? [y,n]:")
+    answer = input("it seems that a download job is already running", aux.get_download_job_filename(vault_name, region, archive_ids), "check the status? [y,n]:")
     if answer.lower() == "y" or answer.lower() == "yes":
-      download_single_archive(archive_id)
-      print("download done, exiting...")
-      sys.exit(0)
+    
+      file = open(aux.get_download_job_filename(vault_name, region, archive_ids), "r")
+      job_id = file.read()
+      file.close()
+
+      # print("exec: aws glacier list-jobs --account-id " + account_id + " --vault-name " + vault_name)
+      aws_jobs = json.loads(subprocess.run(['aws', 'glacier', 'list-jobs', '--account-id', account_id, '--vault-name', vault_name], stdout=subprocess.PIPE).stdout.decode('utf-8').strip('\n'))
+
+      found = False
+
+      for aws_job in aws_jobs["JobList"]:
+        if aws_job["JobId"] != job_id:
+          continue
+
+        found = True
+        if aws_job["StatusCode"] == "InProgress":
+          print("the jobs is still running...")
+          break
+
+        if aws_job["StatusCode"] == "Failed":
+          print("the job has failed!")
+          break
+
+        if aws_job["StatusCode"] == "Succeeded":
+          should_continue = input("the job is successfully finished, do you want to download the archive(s)? [Y/N]: ")
+          if should_continue.lower() == "y" or should_continue.lower() == "yes":
+            for archive_id in archive_ids:
+              download_single_archive(job_id, archive_id)
+            print("download done, exiting...")
+            sys.exit(0)
+      
+      if found == False:
+        print("job not found, probably expired! You have to request it again")
 
  #  aws glacier initiate-job --account-id - --vault-name my-vault --job-parameters file://job-archive-retrieval.json
  #  job-archive-retrieval.json  is  a  JSON  file  in the local folder that
@@ -60,19 +90,21 @@ def start_or_check_retrieval_job(archive_id):
     "ArchiveId": archive_id,
   }
 
-  aws_job_response = json.loads(subprocess.run(['aws', 'glacier', 'initiate-job', '--account-id', account_id, '--vault-name', vault_name, '--job-parameters', json.dumps(request_parameters), job_result_filename(vault_name, region)], stdout=subprocess.PIPE).stdout.decode('utf-8').strip('\n'))
+  aws_job_response = json.loads(subprocess.run(['aws', 'glacier', 'initiate-job', '--account-id', account_id, '--vault-name', vault_name, '--job-parameters', json.dumps(request_parameters)], stdout=subprocess.PIPE).stdout.decode('utf-8').strip('\n'))
 
   job_id = aws_job_response["jobId"]
-
-  print("job for vault " + vault_name + " initiated (job id: "  + job_id + "), job id stored in " + aux.get_download_job_filename(vault_name, region, [archive_id]))
 
   job_file = open(aux.get_download_job_filename(vault_name, region, [archive_id]), "w")
   job_file.write(job_id)
   job_file.close()
 
+  print("job for vault " + vault_name + " initiated (job id: "  + job_id + "), job id stored in " + aux.get_download_job_filename(vault_name, region, [archive_id]))
 
-def download_single_archive(archive_id):
+
+def download_single_archive(job_id, archive_id):
   print("start download request for archive_id:", archive_id)
+
+  subprocess.run(['aws', 'glacier', 'get-job-output', '--account-id', account_id, '--vault-name', vault_name, '--job-id', job_id, aux.get_download_archive_id(vault_name,archive_id)])
 
 
 ########################################
